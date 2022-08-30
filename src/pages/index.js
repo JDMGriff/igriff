dc207e3fafb885322c3f73f7bc6510f897249119
Ian Griffiths <ian.griffiths@multiplefoods.local>
2022-08-30 16:47:56 +0100

    added latest blogs to homepage - refactor to be react component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,47 @@
 // Step 1: Import React
 import * as React from 'react';
+import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import { StaticImage } from 'gatsby-plugin-image';
 import Seo from '../components/seo';
 
 // Step 2: Define your component
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
     return (
         <Layout pageTitle='Home Page'>
             <p>I'm making this by following the Gatsby Tutorial.</p>
             <StaticImage alt='image' src='../images/game-over.jpg' />
+
+            {/* Include latest blogs - refactor to be component */}
+            <h2>Blog Posts</h2>
+            {data.allMdx.nodes.map((node) => (
+                <article key={node.id}>
+                    <h2>
+                        <Link to={`/blog/${node.frontmatter.slug}`}>
+                            {node.frontmatter.title}
+                        </Link>
+                    </h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                </article>
+            ))}
         </Layout>
     );
 };
+
+export const query = graphql`
+    query {
+        allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+            nodes {
+                frontmatter {
+                    date(formatString: "MMMM D, YYYY")
+                    title
+                    slug
+                }
+                id
+            }
+        }
+    }
+`;
 
 export const Head = () => (
     <>
