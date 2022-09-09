cc08584541abb4730afdc4fdcd2d6ca64a2ab98d
Ian Griffiths <ian.griffiths@multiplefoods.local>
2022-09-09 10:58:38 +0100

    updated latest code base

--- src/pages/latest-work/{mdx.frontmatter__slug}.js
+++ src/pages/latest-work/{mdx.frontmatter__slug}.js
@@ -1,42 +1,65 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import Layout from '../../components/layout'
-import Seo from '../../components/seo'
+import * as React from 'react';
+import { graphql } from 'gatsby';
+// import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Layout from '../../components/layout';
+import Seo from '../../components/seo';
 
 const BlogPost = ({ data, children }) => {
-  const image = getImage(data.mdx.frontmatter.hero_image)
-  return (
-    <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
-      <GatsbyImage
-        image={image}
-        alt={data.mdx.frontmatter.hero_image_alt}
-      />      
-      {children}
-    </Layout>
-  )
-}
+    // const image = getImage(data.mdx.frontmatter.hero_image);
+    return (
+        <div>
+            <Layout>
+                <div className='latest-work-item'>
+                    <div className='latest-work-item__hero'>
+                        <h1>{data.mdx.frontmatter.title}</h1>
+                        <div className='latest-work-item__hero--details'>
+                            <p>
+                                Created in:
+                                <span className='xd-icon'>
+                                    {data.mdx.frontmatter.created_in}
+                                </span>
+                            </p>
+                            <p>
+                                Status: <span className='live-status'></span>
+                                {data.mdx.frontmatter.status}
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* <GatsbyImage
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+                /> */}
+                </div>
+            </Layout>
+            <div className='job-summary'>
+                <div className='container'>{children}</div>
+            </div>
+        </div>
+    );
+};
 
 export const query = graphql`
-  query($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        hero_image_alt
-        hero_image_credit_link
-        hero_image_credit_text
-        hero_image {
-          childImageSharp {
-            gatsbyImageData
-          }
+    query ($id: String) {
+        mdx(id: { eq: $id }) {
+            frontmatter {
+                title
+                created_in
+                status
+                date(formatString: "MMMM DD, YYYY")
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
+            }
         }
-      }
     }
-  }
-`
+`;
 
-export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />;
 
-export default BlogPost
+export default BlogPost;
